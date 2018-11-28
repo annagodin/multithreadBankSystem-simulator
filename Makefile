12f491cb97eee6a089abf1c71ad526c5c9102e36
Anna <annagodin24@gmail.com>
2018-11-28 00:57:27 -0500

    beginnings

---
+++ Makefile
@@ -0,0 +1,8 @@
+all: bankingClient bankingServer bankingClient.c bankingServer.c
+
+bankingClient: bankingClient.c bankingClient.o
+	gcc -pthread -Wall -Werror=implicit-function-declaration -o bankingClient bankingClient.o
+bankingServer: bankingServer.o bankingServer.o
+	gcc -pthread -Wall -Werror=implicit-function-declaration -o bankingServer bankingServer.o
+clean:
+	rm -f bankingServer bankingClient
---
+++ bankingClient.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#define PORT 9382
+#define _GNU_SOURCE
+
+int main(int argc, char *argv[]) {
+	printf("this is the banking client\n");
+
+	//----------------------TESTING FROM GEEKS FOR GEEKS-------------------------
+	struct sockaddr_in address; 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *helloMessage = "Hello from the client!!!!!!!! FUCK THREADING AND SYSTEMS"; 
+    char buffer[1024] = {0}; 
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+
+    send(sock , helloMessage , strlen(helloMessage) , 0 ); 
+    printf("Hello message sent\n"); 
+    valread = read( sock , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    
+    //----------------------TESTING FROM GEEKS FOR GEEKS-------------------------
+
+	return 0;
+}
